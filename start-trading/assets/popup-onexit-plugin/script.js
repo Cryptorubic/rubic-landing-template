b72b0e5681c85135183493934c6b101f5159f2ac
axtezy <axtezy@gmail.com>
2021-08-30 11:15:40 +0300

    Start trading page add

---
+++ start-trading/assets/popup-onexit-plugin/script.js
@@ -0,0 +1,3 @@
+
+(function(){document.addEventListener("DOMContentLoaded",function(){if(!document.querySelector("html").classList.contains("is-builder")){var c=!0;document.addEventListener("mouseleave",function(){c&&(document.querySelectorAll(".mbr-popup[data-on-exit]").forEach(function(b){var a;"undefined"!==typeof bootstrap?("undefined"!==typeof bootstrap.Modal.getInstance&&(a=bootstrap.Modal.getInstance(b)),a||(a=new bootstrap.Modal(b),{keyboard:!1}),a.show()):"undefined"!==typeof jQuery&&$(b).modal({keyboard:!1})}),
+c=!1)})}})})();
